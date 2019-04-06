5667369cec3ff69a552f17188e05e2d38368d6af
spencer kelly <spencermountain@gmail.com>
2019-04-06 12:43:00 -0400

    cleanup

--- scratch.js
+++ scratch.js
@@ -1,6 +1,6 @@
 const exec = require('shelljs').exec;
 
 //the quotations here are strangely-important
-let cmd = `tape ~/mountain/nlp-compromise/test/unit/**/*.test.js | ./src/index.js`
+let cmd = `./node_modules/.bin/tape ~/mountain/wtf_wikipedia/tests/**/*.test.js | ./src/index.js`
 console.log(cmd)
 exec(cmd);
--- src/index.js
+++ src/index.js
@@ -1,71 +1,73 @@
 #!/usr/bin/env node
-const tapOut = require('tap-out');
-const chalk = require('chalk');
-const fns = require('./fns');
-// const stacktraceParser = require('stacktrace-parser').parse;
+'use strict'
+const tapOut = require('tap-out')
+const chalk = require('chalk')
+const fns = require('./fns')
 
-const dot = chalk.green('•');
+const dot = chalk.green('•')
 
-const failures = [];
-let passed = 0;
-const start = Date.now();
+const failures = []
+let passed = 0
+const start = Date.now()
 //start us off
-console.log('\n');
-
-// const parse
+console.log('\n')
 
 const listFailures = function() {
   failures.forEach((obj, i) => {
-    console.log('');
-    // let stack = stacktraceParser(obj.error.stack);
-    // console.log(stack);
-    console.log(chalk.red(`   #${i} ${chalk.red('- ' + obj.name + ' -')}`));
-    let msg = '';
-    msg += chalk.cyan(`           ${"'" + obj.error.actual + "'"}`);
-    msg += '\n';
-    msg += chalk.grey(`     want: `) + chalk.magenta(`${"'" + obj.error.expected + "'"}`);
-    console.log(msg);
-  });
-};
+    console.log('')
+    console.log(chalk.red(`   #${i} ${chalk.red('- ' + obj.name + ' -')}`))
+    let msg = ''
+    msg += chalk.cyan(`           ${"'" + obj.error.actual + "'"}`)
+    msg += '\n'
+    msg +=
+      chalk.grey(`     want: `) +
+      chalk.magenta(`${"'" + obj.error.expected + "'"}`)
+    console.log(msg)
+  })
+}
 
 //callback
 const done = function() {
   if (failures.length === 0) {
-    const time = fns.duration(start);
+    const time = fns.duration(start)
     console.log('')
     console.log('   ' + chalk.grey(time + 's'))
-    console.log(chalk.green('   ' + fns.niceNumber(passed) + '  ✔️'));
-    process.exit(0);
+    console.log(chalk.green('   ' + fns.niceNumber(passed) + '  ✔️'))
+    process.exit(0)
   } else {
-    let noun = failures.length === 1 ? 'failure' : 'failures';
-    listFailures();
-    console.log('\n');
-    console.log('           ' + chalk.grey(fns.niceNumber(passed) + ' passed'));
-    console.log(chalk.red(`  ◠◡◜◠◡-◡    ${fns.niceNumber(failures.length)} ${noun}   `));
-    process.exit(1);
+    let noun = failures.length === 1 ? 'failure' : 'failures'
+    listFailures()
+    console.log('\n')
+    console.log('           ' + chalk.grey(fns.niceNumber(passed) + ' passed'))
+    console.log(
+      chalk.red(`  ◠◡◜◠◡-◡    ${fns.niceNumber(failures.length)} ${noun}   `)
+    )
+    process.exit(1)
   }
-};
+}
 
-const t = tapOut(done);
+const t = tapOut(done)
 
 t.on('assert', function(assert) {
   if (assert.ok === true) {
     if (failures.length === 0) {
-      process.stdout.write(dot);
+      process.stdout.write(dot)
     }
-    passed += 1;
-  } else { //failures
-    if (failures.length === 0) { //first failure
-      process.stdout.write(chalk.red(` ✘  (${assert.name})`) + '\n');
-      console.log(chalk.red('          . . .'));
+    passed += 1
+  } else {
+    //failures
+    if (failures.length === 0) {
+      //first failure
+      process.stdout.write(chalk.red(` ✘  (${assert.name})`) + '\n')
+      console.log(chalk.red('          . . .'))
     }
-    failures.push(assert);
+    failures.push(assert)
   }
-});
+})
 
 //support console.logs
 t.on('comment', function(comment) {
-  console.log(comment.raw);
-});
+  console.log(comment.raw)
+})
 
-process.stdin.pipe(t);
+process.stdin.pipe(t)
